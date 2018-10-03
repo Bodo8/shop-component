ba0cd1a4858d3f2ca0f0affcdd2d6e58713d1bff
Bogusław <b.trojanski@gmail.com>
2018-10-03 07:50:42 +0200

    improved description

--- src/main/java/pl/database/impl/sale/SqlDatabase.java
+++ src/main/java/pl/database/impl/sale/SqlDatabase.java
@@ -15,6 +15,9 @@ import pl.repository.ReceiptRepository;
 
 import java.util.List;
 
+/**
+ * SqlDatabase - database int the MySQL database.
+ */
 @Repository
 @ConditionalOnProperty(name = {"active.database"}, havingValue = "sql")
 @Transactional
@@ -26,16 +29,25 @@ public class SqlDatabase implements Database {
   @Autowired
   ProductRepository productRepository;
 
+  /**
+   * @param receipt - saves given receipt into a pl.database.
+   */
   @Override
   public void saveReceipt(Receipt receipt) {
     receiptRepository.save(receipt);
   }
 
+  /**
+   * @param receipt - deletes given receipt into a pl.database.
+   */
   @Override
   public void removeReceipt(Receipt receipt) {
     receiptRepository.delete(receipt);
   }
 
+  /**
+   * @return - generates object Receipt from the ReceiptBody list.
+   */
   @Override
   public Receipt generateReceipt(List<ReceiptBody> receiptBodyList,
       SumsUp sumsUp) {
@@ -44,11 +56,17 @@ public class SqlDatabase implements Database {
             sumsUp);
   }
 
+  /**
+   * @return - all receipt from pl.database.
+   */
   @Override
   public List<Receipt> getAllReceipt() {
     return (List<Receipt>) receiptRepository.findAll();
   }
 
+  /**
+   * @param product - it is single consumer purchase.
+   */
   @Override
   public synchronized void saveProduct(Product product) {
     String productName = product.getProductStore().getBasisProduct().getProductName();
@@ -65,27 +83,42 @@ public class SqlDatabase implements Database {
     productRepository.save(product);
   }
 
+  /**
+   * @return - currently customer purchase.
+   */
   @Override
   public List<Product> getProducts() {
     return (List<Product>) productRepository.findAll();
   }
 
+  /**
+   * looking for a product in the currently purchase.
+   */
   @Override
   public Product getProduct(String nameProduct) {
     List<Product> productsList = (List<Product>) productRepository.findByNameProduct(nameProduct);
     return productsList.get(0);
   }
 
+  /**
+   * deletes product from the currently purchase.
+   */
   @Override
   public void deleteProduct(Product product) {
     productRepository.delete(product);
   }
 
+  /**
+   * clear currently list with Products after save.
+   */
   @Override
   public void clearListWithProductsAfterSave() {
     productRepository.deleteAll();
   }
 
+  /**
+   * changes quantity the purchased of one product.
+   */
   @Override
   public void updateProduct(String productName, Product product) {
     Integer quantityPurchase = product.getQuantityPurchase();
